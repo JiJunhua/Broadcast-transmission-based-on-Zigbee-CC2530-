d0844f38b7ae2ee5d8524b0e74f14b45295788e3
JiJunhua <928176812@qq.com>
2017-07-26 13:54:43 +0800

    Add files via upload

--- CC2530_TR/main.c
+++ CC2530_TR/main.c
@@ -69,76 +69,73 @@ void timer1_enable()
 
 void rf_init()
 {
-  TXPOWER       = 0xF5; //发射功率为4.5dBm（最大）
-  CCACTRL0      = 0xF8; //推荐值 smartRF软件生成 CCA:空闲通道评估
+  TXPOWER       = 0xF5;         //发射功率为4.5dBm（最大）
+  CCACTRL0      = 0xF8;         //推荐值 smartRF软件生成 CCA:空闲通道评估
   
-  FRMFILT0      = 0x0C; //静止接手过滤，即接收所有数据包
+  FRMFILT0      = 0x0C;         //禁止接收过滤，即接收所有数据包
   
-  FSCAL1        = 0x00; //推荐值 smartRF软件生成
+  FSCAL1        = 0x00;         //推荐值 smartRF软件生成
   TXFILTCFG     = 0x09;
   AGCCTRL1      =  0x15;
   AGCCTRL2      =  0xFE;
 
-  TXFILTCFG     = 0x09; // 推荐值 smartRF软件生成
+  TXFILTCFG     = 0x09;         // 推荐值 smartRF软件生成
   
-  FREQCTRL      = 0x0B; // 选择通道11
+  FREQCTRL      = 0x0B;         // 选择通道11
 
   RFIRQM0       |= (1<<6);      // 使能RF数据包接收中断
   IEN2          |= (1<<0);      // 使能RF中断
 
-  RFST          = 0xED; // 清除RF接收缓冲区 ISFLUSHRX
-  RFST          = 0xE3; // RF接收使能 ISRXON
+  RFST          = 0xED;         // 清除RF接收缓冲区 ISFLUSHRX
+  RFST          = 0xE3;         // RF接收使能 ISRXON
 }
 
 void rf_send(char *pbuf , int len)      //射频发送数据
 {
-  RFST = 0xE3;          //RF接受使能 ISRXON
+  RFST = 0xE3;                  //RF接受使能 ISRXON
   //等待发送状态不活跃，并且没有接受到SFD
   while( FSMSTAT1 & (( 1<<1 ) | ( 1<<5 )));
   
-  RFIRQM0 &= ~( 1<<6 ); //禁止接收数据包中断
-  IEN2 &= ~( 1<<0 );    //清除RF全局中断
+  RFIRQM0 &= ~( 1<<6 );         //禁止接收数据包中断
+  IEN2 &= ~( 1<<0 );            //清除RF全局中断
   
-  RFST = 0xEE;          //清除发送缓冲区 ISFLUSHTX
-  RFIRQF1 = ~( 1<<1 );  //清除发送完成标志
+  RFST = 0xEE;                  //清除发送缓冲区 ISFLUSHTX
+  RFIRQF1 = ~( 1<<1 );          //清除发送完成标志
   
   //填充缓冲区，填充过程需要增加2字节，CRC校验自动填充
   RFD = len + 2;
   
-  printf("RFD = %3c\r\n",RFD);
-  printf("RFD = %3d\r\n",RFD);
-  
   for (int i = 0; i < len; i++)
   {
-      RFD = *pbuf++;    //写RFD，相当于写发送缓冲区
+      RFD = *pbuf++;            //写RFD，相当于写发送缓冲区
   }
   
-  RFST = 0xE9;          //发送数据包 ISTXON
+  RFST = 0xE9;                  //发送数据包 ISTXON
   while (!(RFIRQF1 & ( 1<<1 )));        //等待发送完成
-  RFIRQF1 = ~( 1<<1 );  //清除发送完成标志位
+  RFIRQF1 = ~( 1<<1 );          //清除发送完成标志位
   
-  RFIRQM0 |= ( 1<<6 );  //RX接收中断
-  IEN2 |= ( 1<<0 );     //RF一般中断使能
+  RFIRQM0 |= ( 1<<6 );          //RX接收中断
+  IEN2 |= ( 1<<0 );             //RF一般中断使能
   
 }
 
-void rf_receive_isr()   //接收数据包
+void rf_receive_isr()           //接收数据包
 {
   int rf_rx_len = 0;
   int rssi = 0;
   char crc_ok = 0;
   
-  rf_rx_len = RFD - 2;  //长度去除2字节的附加结果
-  rf_rx_len &= 0x7F;    //长度不能超过128
+  rf_rx_len = RFD - 2;          //长度去除2字节的附加结果
+  rf_rx_len &= 0x7F;            //长度不能超过128
   for (int i = 0; i < rf_rx_len; i++)
   {
     rf_rx_buf[i] = RFD;         //连续读取缓冲区的内容
   }
   
-  rssi = RFD - 73;      //读取RSSI结果 ？？？？？
-  crc_ok = RFD;         //去读crc校验结果 BIT7
+  rssi = RFD - 73;              //读取RSSI结果 ？？？？？
+  crc_ok = RFD;                 //去读crc校验结果 BIT7
   
-  RFST = 0xED;          //清除接收缓冲区 
+  RFST = 0xED;                  //清除接收缓冲区 
   if( crc_ok & 0x80)
   {
     uart0_sendbuf( rf_rx_buf , rf_rx_len);      //串口发送
@@ -162,11 +159,11 @@ void main(void)
   CLKCONCMD &= ~0x47;
   SLEEPCMD = 0x04;
   
-  uart0_init();         //串口初始化
-  timer1_init();        //定时器初始化 2ms 比较匹配
-  rf_init();            //RF初始化 无帧过滤
+  uart0_init();                 //串口初始化
+  timer1_init();                //定时器初始化 2ms 比较匹配
+  rf_init();                    //RF初始化 无帧过滤
   
-  EA = 1;               //使能全局中断
+  EA = 1;                       //使能全局中断
   
   printf("The system is running....\r\n");
   
